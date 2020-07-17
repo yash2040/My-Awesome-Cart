f71911955f5239c44a74b70cc85b349043dcafea
Yash Khandelwal <68386161+yash2040@users.noreply.github.com>
2020-07-17 11:59:19 +0530

    Add files via upload

---
+++ mac/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from math import ceil
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+def index(request):
+    return render(request,'index.html')
+def loginuser(request):
+
+    if request.method=="POST":
+        name=request.POST.get('username')
+        password=request.POST.get('password')
+        print(name,password)
+        user = authenticate(username=name, password=password)
+        if user is not None:
+            print("haskhfl")
+            return redirect("http://127.0.0.1:8000/shop/")
+            # A backend authenticated the credentials
+        else:
+            messages.error(request,"Please Enter your details correctly")
+            return render(request, 'loginuser.html')
+            # No backend authenticated the credentials
+    return render(request,'loginuser.html')
+def signinuser(request):
+    if request.method == "POST":
+        uname = request.POST.get('username')
+        password = request.POST.get('password')
+        email=request.POST.get('email')
+        lname = request.POST.get('lname')
+        fname=request.POST.get('fname')
+        user1 = authenticate(username=uname, password=password)
+        if user1 is None:
+            user = User.objects.create_user(uname, email, password)
+            user.last_name = lname
+            user.first_name=fname
+            user.save()
+            messages.success(request,"Account Created Successfully.Please LogIn")
+        else:
+            messages.error(request,"Username Already taken.Please Choose different user name")
+    return render(request, 'signinuser.html')
+def logoutuser(request):
+    return render(request, 'logoutuser.html')
